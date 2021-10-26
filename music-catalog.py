97a0839185c5853f16d65636d02c4aa05b95311d
ddch42 <85430824+dch42@users.noreply.github.com>
2021-10-25 19:57:02 -0500

    use BLAKE2B instead of MD5

--- music-catalog.py
+++ music-catalog.py
@@ -7,7 +7,7 @@ date = datetime.now().date()
 time = datetime.now().time().strftime('%H-%M-%S')
 
 path = sys.argv[1]
-hasher = hashlib.md5()
+hasher = hashlib.blake2b()
 extensions = [".mp3", ".m4a"]
 
 db = sqlite3.connect("./data/music_library.db")
@@ -26,31 +26,36 @@ def menu_loop(menu):
         if choice in menu:
             menu[choice][1]()
 
+def go_back(question):
+    go_back = None
+    while go_back != 'y':
+        go_back = input(question).lower().strip()
+
 def iter_music(path, db, cursor, hasher):
     """Scan for audio files and add to database"""
     for root, dirs, files in os.walk(path, topdown=False):
         for filename in files:
                 if filename.endswith(tuple(extensions)):
                     audio_file = os.path.join(root, filename)
-                    print("\nCreating hash for %s..." % filename)
+                    print("\nHashing  \033[4m%s\033[0m..." % filename)
                     with open(audio_file, 'rb') as file_to_hash:
                         buf = file_to_hash.read()
                         hasher.update(buf)
-                        md5 = str(hasher.hexdigest())
-                        print("MD5 HASH:", md5)
-                    print("Parsing tag data from %s..." % filename)
+                        blake2b_hash = str(hasher.hexdigest())
+                        print("\033[1m\033[96mBLAKE2B HASH:\033[0m\033[0m", blake2b_hash)
+                    print("Parsing tag data from \033[4m%s\033[0m..." % filename)
                     audio_obj = eyed3.load("%s" % audio_file)
                     try:
-                        add_to_db(audio_file, audio_obj, db, cursor, md5)
-                    except:
-                        print("Hash exists, skipping file...")
-                        pass
+                        add_to_db(audio_file, audio_obj, db, cursor, blake2b_hash)
+                    except Exception as e: print("\033[91m", e, "\033[0m")
+
     print("DONE!")
     go_back = None
     while go_back != 'y':
         go_back = input('Go back to menu? y/N: ').lower().strip()
 
-def add_to_db(audio_file, audio_obj, db, cursor, md5):
+def add_to_db(audio_file, audio_obj, db, cursor, blake2b_hash):
+    """Insert parsed info into database"""
     cursor.execute("""CREATE TABLE IF NOT EXISTS music_info(
                 Hash TEXT PRIMARY KEY,
                 Album_Artist TEXT,
@@ -66,7 +71,7 @@ def add_to_db(audio_file, audio_obj, db, cursor, md5):
                 """)
 
     song_info = [(
-        md5,
+        blake2b_hash,
     str(audio_obj.tag.album_artist),
     str(audio_obj.tag.artist), 
     str(audio_obj.tag.getBestDate()), 
@@ -81,7 +86,7 @@ def add_to_db(audio_file, audio_obj, db, cursor, md5):
     cursor.executemany("INSERT INTO music_info VALUES (?,?,?,?,?,?,?,?,?,?,?)", song_info)
     db.commit()
 
-    print("Success: Record inserted to the table!")
+    print("Success: Info for %s - %s inserted to the table!" % (str(audio_obj.tag.artist), str(audio_obj.tag.title)))
 
 def search_db(cursor):
     """Search music database"""
@@ -97,9 +102,7 @@ def export_all(db):
     music_df = pd.read_sql_query("SELECT * FROM music_info", db)
     music_df.to_csv('data/csv_exports/music_database-%s-%s.csv' % (date, time), index=False)
     print("Exported to 'data/csv_exports/music_database-%s-%s.csv'" % (date, time))
-    go_back = None
-    while go_back != 'y':
-        go_back = input('Go back to menu? y/N: ').lower().strip()
+    go_back('Go back to menu? y/N: ')
 
 def export_albums(db):
     """Export album info to csv"""
@@ -107,9 +110,8 @@ def export_albums(db):
     music_df = music_df.drop_duplicates()
     music_df.to_csv('data/csv_exports/albums_database-%s-%s.csv' % (date, time), index=False)
     print("Exported to 'data/csv_exports/albums_database-%s-%s.csv'" % (date, time))
-    go_back = None
-    while go_back != 'y':
-        go_back = input('Go back to menu? y/N: ').lower().strip()
+    go_back('Go back to menu? y/N: ')
+
 
 
 main_menu = {
